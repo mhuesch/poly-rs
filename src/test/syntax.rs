9c37193bfe63668c9e82581ae1c73f0c691062a7
Michael Hueschen <m@mhueschen.space>
2020-10-20 21:52:18 -0700

    improve `Let` & `If` shrinkers
    shrink to subexpressions and their shrinks.

--- src/test/syntax.rs
+++ src/test/syntax.rs
@@ -30,17 +30,27 @@ impl Arbitrary for Expr {
             }
             Expr::Let(nm, e, bd) => {
                 let nm_ = nm.clone();
-                Box::new(
-                    (e.clone(), bd.clone())
-                        .shrink()
-                        .map(move |(e_, bd_)| Expr::Let(nm_.clone(), e_, bd_)),
-                )
-            }
-            Expr::If(tst, thn, els) => Box::new(
-                (tst.clone(), thn.clone(), els.clone())
+                let pairs = (e.clone(), bd.clone())
                     .shrink()
-                    .map(|(tst_, thn_, els_)| Expr::If(tst_, thn_, els_)),
-            ),
+                    .map(move |(e_, bd_)| Expr::Let(nm_.clone(), e_, bd_));
+                let es = single_shrinker(*e.clone())
+                    .chain(e.shrink().map(|v| *v));
+                let bds = single_shrinker(*bd.clone())
+                    .chain(bd.shrink().map(|v| *v));
+                Box::new(pairs.chain(es).chain(bds))
+            }
+            Expr::If(tst, thn, els) => {
+                let pairs = (tst.clone(), thn.clone(), els.clone())
+                    .shrink()
+                    .map(|(tst_, thn_, els_)| Expr::If(tst_, thn_, els_));
+                let tsts = single_shrinker(*tst.clone())
+                    .chain(tst.shrink().map(|v| *v));
+                let thns = single_shrinker(*thn.clone())
+                    .chain(thn.shrink().map(|v| *v));
+                let elss = single_shrinker(*els.clone())
+                    .chain(els.shrink().map(|v| *v));
+                Box::new(pairs.chain(tsts).chain(thns).chain(elss))
+            }
             Expr::Fix(bd) => Box::new(bd.shrink().map(|bd_| Expr::Fix(bd_))),
             _ => empty_shrinker(),
         }
