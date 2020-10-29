4c640460fbf74eab2d1f7ed30c135f3f181b3d6d
Michael Hueschen <m@mhueschen.space>
2020-10-29 13:08:11 -0700

    make a repl which parses & typechecks `Expr`s

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
 use combine::parser::Parser;
 use combine::stream::easy;
+use rustyline::{error::ReadlineError, Editor};
+
 use poly::{env::*, infer::*, parse::expr};
 
 fn main() {
     println!("hello, poly & Rust!");
 
-    let prog = "((lam [x] x) 1)";
-    let e = match expr().parse(easy::Stream(prog)) {
-        Ok((e, _)) => e,
-        _ => panic!("wat"),
-    };
-    println!("{:?}", e);
-
-    let env = Env::new();
-    let res = constraints_expr(env, e);
-    println!("{:?}", res);
+    let mut rl = Editor::<()>::new();
+    loop {
+        let readline = rl.readline("> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                match expr().parse(easy::Stream(&line[..])) {
+                    Err(err) => println!("parse error: {}", err),
+                    Ok((e, _)) => {
+                        println!("{:?}", e);
+                        let env = Env::new();
+                        let res = constraints_expr(env, e);
+                        println!("{:?}", res);
+                    }
+                };
+            }
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
+                println!("\nbye!");
+                break;
+            }
+            Err(err) => {
+                println!("error: {:?}", err);
+                break;
+            }
+        }
+    }
 }
